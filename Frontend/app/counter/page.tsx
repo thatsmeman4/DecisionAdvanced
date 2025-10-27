65007342da1eac205c0b870583d52c9555dd3f52
thatsmeman4 <thatsmeman@proton.me>
2025-10-27 20:20:24 +0100

    Add page.tsx

---
+++ Frontend/app/counter/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { FHECounterDemo } from "@/components/FHECounterDemo";
+
+// Demo sử dụng các component UI cần thiết (Button, Card, Input...)
+export default function TestZamaPage() {
+  return (
+    <div>
+      <FHECounterDemo></FHECounterDemo>
+    </div>
+  );
+}
