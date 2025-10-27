49aacafdceac9917135819c4020093dd7d7d1076
thatsmeman4 <thatsmeman@proton.me>
2025-10-27 20:20:21 +0100

    Add FHECounterAddresses.ts

---
+++ Frontend/abi/FHECounterAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const FHECounterAddresses = { 
+  "11155111": { address: "0xAC899AD001A04c18863F33294B375Fa89EC1d3B3", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0xAC899AD001A04c18863F33294B375Fa89EC1d3B3", chainId: 31337, chainName: "hardhat" },
+};
