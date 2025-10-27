e833be11d22c3caba61b94a8334f802f2365bea2
thatsmeman4 <thatsmeman@proton.me>
2025-10-27 20:20:23 +0100

    Add VotingRoomAddresses.ts

---
+++ Frontend/abi/VotingRoomAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const VotingRoomAddresses = { 
+  "11155111": { address: "0x28eD335833DD3Ecc88a03d98E99a370c44f95bCD", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x28eD335833DD3Ecc88a03d98E99a370c44f95bCD", chainId: 31337, chainName: "hardhat" },
+};
